cb0de06a075de0e32d019d61dd1909e1c22b3b80
Fedor Indutny <fedor.indutny@gmail.com>
2012-01-22 17:48:29 +0600

    bench: improve benchmark

--- test/one-thread-bench.cc
+++ test/one-thread-bench.cc
@@ -2,36 +2,48 @@
 
 TEST_START("one thread benchmark", "one-thread-bench")
 
-  const int num = 100000;
-  int i;
+  const int num = 500000;
+  const int value_len = 1000;
+  const int delta = 20000;
+  int i, start;
 
-  char keys[num][20];
+  char keys[num][100];
+  char value[value_len];
 
   /* init keys */
   for (int i = 0; i < num; i++) {
-    sprintf(keys[i], "string: %d", i);
+    sprintf(keys[i], "big key value lakkaka: %d", i);
   }
+  /* init value */
+  for (int i = 0; i < value_len; i++) {
+    value[i] = 'a' + (rand() % 52);
+  }
+  value[value_len - 1] = 0;
 
-  BENCH_START(write, num)
-  for (int i = 0; i < num; i++) {
-    bp_sets(&db, keys[i], keys[i]);
-  }
-  BENCH_END(write, num)
+  for (start = 0; start < num; start += delta) {
+    fprintf(stdout, "%d items in db\n", start);
 
-  BENCH_START(read, num)
-  for (int i = 0; i < num; i++) {
-    char* value;
-    bp_gets(&db, keys[i], &value);
-    free(value);
+    BENCH_START(write, delta)
+    for (i = start; i < start + delta; i++) {
+      bp_sets(&db, keys[i], value);
+    }
+    BENCH_END(write, delta)
+
+    BENCH_START(read, delta)
+    for (i = start; i < start + delta; i++) {
+      char* value1;
+      bp_gets(&db, keys[i], &value1);
+      free(value1);
+    }
+    BENCH_END(read, delta)
   }
-  BENCH_END(read, num)
 
   BENCH_START(compact, 0)
   bp_compact(&db);
   BENCH_END(compact, 0)
 
   BENCH_START(read_after_compact, num)
-  for (int i = 0; i < num; i++) {
+  for (i = 0; i < num; i++) {
     char* value;
     bp_gets(&db, keys[i], &value);
     free(value);
@@ -39,7 +51,7 @@ TEST_START("one thread benchmark", "one-thread-bench")
   BENCH_END(read_after_compact, num)
 
   BENCH_START(remove, num)
-  for (int i = 0; i < num; i++) {
+  for (i = 0; i < num; i++) {
     bp_removes(&db, keys[i]);
   }
   BENCH_END(remove, num)
